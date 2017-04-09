548aa02f6d528dda3e281187e626b0dbd4aa893a
ygtxr1997 <gavingreenson@icloud.com>
2017-04-09 16:10:33 +0800

    新增了ID功能
    判断用户是否存在：
           如果存在，问好，结束；
           如果不存在，询问用户是否需要完善信息：
                  如果需要，完善后修改JSON文件，结束；
                  如果不需要，直接结束。

---
+++ 个性化聊天/User.js
@@ -0,0 +1,106 @@
+/*-----------------------------------------------------------------------------
+    这个机器人可以用来记录用户的名字，并把用户的信息保存在同一目录下的 UserInfo.json
+文件中。
+
+    主对话包括两个瀑布流，第一个询问用户的名字，第二个调用 /userLoging 对话。
+    /userLogin 对话也包括两个瀑布流，第一个用来确认 JSON 文件中是否包含这名用户，如果
+包含这个用户，则向用户问好并且返回；如果不包含这个用户，则根据用户先前的回答更新 JSON 
+文件。
+
+    更新 JSON 文件的步骤：
+        #1  将 JSON 文件转化为 JavaScript 对象
+        #2  给 JavaScript 对象添加、修改、删除属性
+        #3  将修改后的 JavaScript 对象转化为一个字符串
+        #4  使用 fs 文件操作，把字符串写入 JSON 文件
+
+    使用方式：
+        说：  hello
+        说：  yuange 或者 lubin 或者 其他未在 JSON 文件中存在的名字
+------------------------------------------------------------------------------*/
+
+// create builder, connector and bot
+var builder = require('botbuilder');
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
+
+// write and read the .json file
+var UserInfo = require('./UserInfo.json');
+var fs = require("fs");
+
+// main dialog
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Who are you, my friend ?');
+    },
+    function (session, results) {
+        session.userData.userName = results.response;
+        session.beginDialog('/userLogin', results.response);
+    },
+    function (session, results) {
+        if (results.response == 'NotExist') {
+            session.beginDialog('/completeInfo', session.userData.userName);
+        } else if (results.response == 'Exist') {
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // session.send("Debug response : " + results.response);
+        if (results.response == 'Y') {
+            // session.send("Debug: Y");
+            session.beginDialog('./completeID', session.userData.userName);
+        } else if (results.response == 'N') {
+            // session.send("Debug: N");
+            var str = JSON.stringify(UserInfo);
+            fs.writeFileSync('./UserInfo.json', str);
+        }
+    }
+]);
+
+// userLogin dialog
+bot.dialog('/userLogin', [
+    function (session, args, next) {
+        session.dialogData.userName = args;     // 'args' is 'result.response' passed by main dialog
+        if (UserInfo.hasOwnProperty(args)) {    // if current user exists
+            session.send("Hello " + args + "!");
+            session.endDialogWithResult({response : "Exist"})
+        } else {                                // current user doesn't exist
+            session.send(session.dialogData.userName + " , I am initialing your information ...")
+
+            // use  eval("xxx.xx" + "=" + "x");  to add properties 
+            eval("UserInfo." + session.dialogData.userName + "=" + " { \"name\" : \"" + session.dialogData.userName + "\" } ");
+
+            session.endDialogWithResult({response : "NotExist"});
+        }
+    }
+]);
+
+// completeInfo dialog
+bot.dialog('/completeInfo', [
+    function (session, args) {
+        builder.Prompts.text(session, "Do you want to add more information like ID or courses , " + args + ' ?');
+    },
+    function (session, results) {
+        var flag;
+        // session.send("Debug results :" + results.response);
+        if (results.response == 'yes') {
+            flag = 'Y';
+        } else {
+            flag = 'N';
+        }
+        session.endDialogWithResult({response : flag});
+    }
+]);
+
+bot.dialog('./completeID',[
+    function (session, args) {
+        session.dialogData.userName = args;
+        builder.Prompts.text(session, "Tell me your ID please .");
+    },
+    function (session, results) {
+        eval("UserInfo." + session.dialogData.userName + ".ID=\"" + results.response + "\"");
+        var str = JSON.stringify(UserInfo);
+        fs.writeFileSync('./UserInfo.json', str);
+        session.send("OK, I finished it .");
+        session.endDialog();
+    }
+]);

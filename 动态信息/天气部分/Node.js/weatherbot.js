9b20f758e239d14d0bb2b8b6717148d26db3364e
watercow <2540837725@qq.com>
2017-04-12 16:35:17 +0800

    更新天气查询功能
    增加了LUIS的自然语言处理部分

--- 动态信息/天气部分/Node.js/weatherbot.js
+++ 动态信息/天气部分/Node.js/weatherbot.js
@@ -1,72 +1,12 @@
+
+/*-----------------Weather Search-----------------------------
+ *   模块名: 天气查询模块
+ *   功能说明：用于在用户输入如“查询天气”时返还天气信息
+-------------------------------------------------------------*/
 var builder = require('botbuilder');
 var restify = require('restify');
 
-//将请求传递给服务端
-//var https = require('https');
-//var querystring = require('querystring');
-
 var returnWeather = require('./returnWeather.js');
-
-//var model = process.env.LUIS_MODEL;
-//var recognizer = new builder.LuisRecognizer(model);
-//var dialog = new builder.IntentDialog( {recognizers: [recognizer]} );
-
-
-
-// module.exports = dialog
-//     .matches('LoadProfile',[
-//         confirmUsername, getProfile
-//     ])
-//     .matches('SearchProfile',[
-//         confirmQuery, searchProfiles, getProfile
-//     ])
-//     .matches('查询天气',[
-//         confirmQueryAddr, searchAddrWeather, getAddrWeather
-//     ])
-//     .onDefault([sendInstructions, redirectConversation]);
-
-
-// //瀑布流询问查询天气
-// function confirmQueryAddr(session, args, next) {
-
-//     session.dialogData.entities = args.entities;
-//     var query  = builder.EntityRecognizer.findEntity(args.entities, '天气');
-
-//     if(query){
-//         next( { response: query.entity } );
-//     }
-//     else{
-//         builder.Prompts.text(session, "sorry，没听清，您要查询哪里的天气？");
-//     }
-// }
-
-// //瀑布流在用户输入中找出要查询的地址
-// function searchAddrWeather(session, results, next) {
-//     var query = session.dialogData.query = results.response;
-//     if( !query ) {
-//         session.endDialog('Request cancelled');
-//     }
-//     else{
-//         //查询天气API接口的使用
-
-//         //github API接口的使用
-//         executeSearch(query , function (profiles) {
-//             var totalCount = profiles.total_count;
-//             if(totalCount == 0){
-//                 session.endDialog('Sorry, no results found');
-//             }
-//             else if(totalCount > 10) {
-//                 session.endDialog('More than 10 results found, please more detail');
-//             }
-//             else {
-//                 session.dialogData.property = null;
-//                 var thumbnails = profiles.items.map();
-//                 var message = new builder.Message(session).attachments(thumbnails);
-//                 session.send(message);
-//             }
-//         });
-//     }
-// }
 
 var connector = new builder.ChatConnector();
 var bot = new builder.UniversalBot(connector);
@@ -97,10 +37,11 @@ dialog.matches(/^search/i , [
                 }
                 else {
                     session.dialogData.property = null;
-                    var weather = profiles.HeWeather5[0].daily_forecast[0].cond.txt_d;
-                    // 老哥，forecast把t打成了e <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<!!!
-                    // 23333
-                    next( { response: weather } );
+                    //JSON解析
+                    var weatherstatus = profiles.HeWeather5[0].daily_forecast[0].cond.txt_d;
+                    var temperature = profiles.HeWeather5[0].now.tmp;
+                    var suggestion = profiles.HeWeather5[0].suggestion.sport.txt;
+                    next( { response: weatherstatus + ' ! 当前有' + temperature + '度 ' + suggestion } );
                 }
             });
         }
